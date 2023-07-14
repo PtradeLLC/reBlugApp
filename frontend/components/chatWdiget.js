373589166d48e7a3188a4b7a4265d03666802693
PublicTrades <info@publictrades.com>
2023-07-14 18:18:10 -0400

    work in progress y

--- frontend/components/chatWdiget.js
+++ frontend/components/chatWdiget.js
@@ -1,22 +1,22 @@
-import React from "react";
+import { useEffect } from "react";
 
-const ChatWidget = () => {
-  const initializeBrevoConversations = (d, w, c) => {
-    w.BrevoConversationsID = "64ae423dc9574471816653f0";
-    w[c] =
-      w[c] ||
-      function () {
-        (w[c].q = w[c].q || []).push(arguments);
-      };
-    var s = d.createElement("script");
-    s.async = true;
-    s.src = "https://conversations-widget.brevo.com/brevo-conversations.js";
-    if (d.head) d.head.appendChild(s);
-  };
+const BrevoConversations = () => {
+  useEffect(() => {
+    (function (d, w, c) {
+      w.BrevoConversationsID = "64ae423dc9574471816653f0";
+      w[c] =
+        w[c] ||
+        function () {
+          (w[c].q = w[c].q || []).push(arguments);
+        };
+      var s = d.createElement("script");
+      s.async = true;
+      s.src = "https://conversations-widget.brevo.com/brevo-conversations.js";
+      if (d.head) d.head.appendChild(s);
+    })(document, window, "BrevoConversations");
+  }, []);
 
-  initializeBrevoConversations(document, window, "BrevoConversations");
-
-  return <div></div>;
+  return null;
 };
 
-export default ChatWidget;
+export default BrevoConversations;
