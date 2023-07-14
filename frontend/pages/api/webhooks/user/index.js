6d0f9b0820bce12c77a12cf55caf9e5fe7c89061
PublicTrades <info@publictrades.com>
2023-07-14 01:14:52 -0400

    removing svix

--- frontend/pages/api/webhooks/user/index.js
+++ frontend/pages/api/webhooks/user/index.js
@@ -1,27 +1,35 @@
-import { Webhook } from "svix";
-import { buffer } from "micro";
+import React from "react";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
+const Index = () => {
+  return <div>Index</div>;
 };
 
-const secret = "whsec_MfKQ9r8GKYqrTwjUPD8ILPZIo2LaLaSw";
+export default Index;
 
-export default async function handler(req, res) {
-  const payload = (await buffer(req)).toString();
-  const headers = req.headers;
+// import { Webhook } from "svix";
+// import { buffer } from "micro";
 
-  const wh = new Webhook(secret);
-  let msg;
-  try {
-    msg = wh.verify(payload, headers);
-  } catch (err) {
-    res.status(400).json({});
-  }
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-  // Do something with the message...
+// const secret = "whsec_MfKQ9r8GKYqrTwjUPD8ILPZIo2LaLaSw";
 
-  res.json({});
-}
+// export default async function handler(req, res) {
+//   const payload = (await buffer(req)).toString();
+//   const headers = req.headers;
+
+//   const wh = new Webhook(secret);
+//   let msg;
+//   try {
+//     msg = wh.verify(payload, headers);
+//   } catch (err) {
+//     res.status(400).json({});
+//   }
+
+//   // Do something with the message...
+
+//   res.json({});
+// }
