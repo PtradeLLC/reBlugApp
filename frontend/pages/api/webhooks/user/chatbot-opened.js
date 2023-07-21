7824f43ff5fcbc07fd3015fc24f44036ced59afb
PublicTrades <info@publictrades.com>
2023-07-21 15:45:47 -0400

    updated path to OpenStream

--- frontend/pages/api/webhooks/user/chatbot-opened.js
+++ frontend/pages/api/webhooks/user/chatbot-opened.js
@@ -1,4 +1,4 @@
-import OpenAIStream from "../../../forgedMartchatbot/utils/OpenAIStream";
+import OpenAIStream from "../../../../../forgedMartchatbot/utils/OpenAIStream";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
