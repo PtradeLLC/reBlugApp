25ef4bd326a1f5626545bc4ec17d4978d812162b
PublicTrades <info@publictrades.com>
2023-07-26 14:21:11 -0400

    updated Tools

--- frontend/components/Hero.js
+++ frontend/components/Hero.js
@@ -54,8 +54,8 @@ export default function Hero() {
               <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                 <div className="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                   <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                    Social automation tools for eCommerce brands to reach
-                    broader audience and boost sales through social media.
+                    Automation tools for eCommerce brands to reach broader
+                    audience and boost sales through social media.
                   </h1>
                   <p className="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
                     <span className="font-extrabold">Just Launched:</span> Email
