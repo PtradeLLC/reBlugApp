2ae4ea77ae28134c3e6d70fe287a5bd55470b275
PublicTrades <info@publictrades.com>
2023-07-28 01:37:22 -0400

    updated ui and navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,6 +1,5 @@
-import { Fragment } from "react";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Disclosure, Menu } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
 import { UserButton, useAuth } from "@clerk/nextjs";
@@ -12,7 +11,7 @@ function classNames(...classes) {
 export default function Navbar() {
   const { isLoaded, userId, sessionId, getToken } = useAuth();
   return (
-    <Disclosure as="nav" className="bg-white shadow">
+    <Disclosure as="nav" className="bg-white inset-x-0 top-0 z-10 fixed shadow">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -30,10 +29,9 @@ export default function Navbar() {
                   </Link>
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                  {/* Current: "border-red-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                   <Link
                     href={"/creators"}
-                    className="inline-flex items-center border-b-2 border-red-500 px-1 pt-1 text-sm font-medium text-gray-900"
+                    className="inline-flex items-center border-b-2 border-red-50 px-1 pt-1 text-sm font-medium text-gray-900"
                   >
                     Creators
                   </Link>
@@ -56,24 +54,6 @@ export default function Navbar() {
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
-                  {/* <div>
-                    <Menu.Button className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-
-                      <UserButton afterSignOutUrl="/" />
-                      {!userId ? (
-                        <a
-                          href="sign-in"
-                          className="text-sm font-semibold leading-6 text-gray-900"
-                        >
-                          Log in <span aria-hidden="true">&rarr;</span>
-                        </a>
-                      ) : (
-                        <UserButton afterSignOutUrl="/" />
-                      )}
-                    </Menu.Button>
-                  </div> */}
                   <div>
                     {!userId ? (
                       <a
@@ -143,134 +123,3 @@ export default function Navbar() {
     </Disclosure>
   );
 }
-
-// import React, { useState } from "react";
-// import { Dialog } from "@headlessui/react";
-// import Image from "next/image";
-// import Link from "next/link";
-// import { Disclosure, Menu, Transition } from "@headlessui/react";
-// import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-// import { UserButton, useAuth } from "@clerk/nextjs";
-
-// const navigation = [
-//   { name: "Creators", href: "/creators" },
-//   // { name: "Agencies", href: "#" },
-//   { name: "Contact Us", href: "/contact" },
-// ];
-
-// const Navbar = () => {
-//   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-//   const { isLoaded, userId, sessionId, getToken } = useAuth();
-//   return (
-//     <div>
-//       <header className="inset-x-0 top-0 z-10 backdrop-blur-md fixed">
-//         <nav
-//           className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-//           aria-label="Global"
-//         >
-//           <div className="sm:mt-2 pr-16">
-//             <Link href={`/`}>
-//               <Image
-//                 src="/images/Mart.png"
-//                 alt="ForgedMart Logo"
-//                 width={100}
-//                 height={24}
-//                 priority
-//               />
-//             </Link>
-//           </div>
-//           <div className="flex lg:hidden">
-//             <button
-//               type="button"
-//               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-//               onClick={() => setMobileMenuOpen(true)}
-//             >
-//               <span className="sr-only">Open main menu</span>
-//             </button>
-//           </div>
-//           <div className="hidden lg:flex lg:gap-x-12">
-//             {navigation.map((item) => (
-//               <a
-//                 key={item.name}
-//                 href={item.href}
-//                 className="text-sm font-semibold leading-6 text-gray-900"
-//               >
-//                 {item.name}
-//               </a>
-//             ))}
-//           </div>
-//           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-//             {!userId ? (
-//               <a
-//                 href="sign-in"
-//                 className="text-sm font-semibold leading-6 text-gray-900"
-//               >
-//                 Log in <span aria-hidden="true">&rarr;</span>
-//               </a>
-//             ) : (
-//               <UserButton afterSignOutUrl="/" />
-//             )}
-//           </div>
-//         </nav>
-//         <Dialog
-//           as="div"
-//           className="lg:hidden"
-//           open={mobileMenuOpen}
-//           onClose={setMobileMenuOpen}
-//         >
-//           <div className="fixed inset-0 z-50" />
-//           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-//             <div className="flex items-center justify-between">
-//               <div className="sm:mt-2 pr-2">
-//                 <Image
-//                   src="/images/Mart.png"
-//                   alt="ForgedMart Logo"
-//                   width={100}
-//                   height={24}
-//                   priority
-//                 />
-//               </div>
-//               <button
-//                 type="button"
-//                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
-//                 onClick={() => setMobileMenuOpen(false)}
-//               >
-//                 <span className="sr-only">Close menu</span>
-//               </button>
-//             </div>
-//             <div className="mt-6 flow-root">
-//               <div className="-my-6 divide-y divide-gray-500/10">
-//                 <div className="space-y-2 py-6">
-//                   {navigation.map((item) => (
-//                     <a
-//                       key={item.name}
-//                       href={item.href}
-//                       className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-//                     >
-//                       {item.name}
-//                     </a>
-//                   ))}
-//                 </div>
-//                 <div className="py-6">
-//                   <UserButton afterSignOutUrl="/" />
-//                   {!userId ? (
-//                     <a
-//                       href="sign-in"
-//                       className="text-sm font-semibold leading-6 text-gray-900"
-//                     >
-//                       Log in <span aria-hidden="true">&rarr;</span>
-//                     </a>
-//                   ) : (
-//                     <UserButton afterSignOutUrl="/" />
-//                   )}
-//                 </div>
-//               </div>
-//             </div>
-//           </Dialog.Panel>
-//         </Dialog>
-//       </header>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
