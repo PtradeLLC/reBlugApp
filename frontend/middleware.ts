1525386ec558b762cd896426018afe081ba70e4a
PublicTrades <info@publictrades.com>
2023-08-09 14:53:01 -0400

    fix for midware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getSession } from "next-auth/react";
 
 export async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname;
-  const session = !!req.cookies.get("next-auth.session-token");
+  const session = await getSession();
 
   if (!session) {
     return NextResponse.redirect(new URL(`pages/login`, req.url));
