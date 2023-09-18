3da840075da9e2c3b3961e3addf0ffaac86c0808
PublicTrades <info@publictrades.com>
2023-09-18 17:22:13 -0400

    updated login

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -12,6 +12,7 @@ export default function Login() {
     const [registerMessage, setRegisterMessage] = useState('');
     const providers = ['Facebook', 'Twitch', 'Google', 'LinkedIn'];
     const { isLoading, isSuccess, isError, error } = useSignInEmailPassword();
+    const [Loading, setLoading] = useState(false);
 
     const nhost = new NhostClient({
         subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN,
@@ -34,16 +35,8 @@ export default function Login() {
     };
 
     if (isLoading) {
-        return (
-            <div className='flex justify-center items-center'>
-                <Loading size="lg" />
-            </div>
-        )
-    }
-
-    if (isError) {
-        return error;
-    }
+        setLoading(true)
+    };
 
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -51,6 +44,9 @@ export default function Login() {
                 <h2 className="mt-12 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
                     Sign in to your account
                 </h2>
+            </div>
+            <div className='flex justify-center items-center'>
+                {Loading ? <Loading size="lg" /> : isError ? <p className="bg-red-200 rounded text-center m-auto px-2">{error?.message}</p> : null}
             </div>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
@@ -97,18 +93,4 @@ export default function Login() {
             </div>
         </div>
     );
-
 }
-
-// export async function getServerSideProps(context) {
-//     const session = await getServerSession(context.req, context.res, authOptions);
-
-//     const providers = await getProviders();
-
-//     return {
-//         props: {
-//             session,
-//             providers: providers ?? []
-//         },
-//     }
-// };
