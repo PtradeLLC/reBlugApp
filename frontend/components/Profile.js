65dae744ae0fe642cbaa15c16980e08b3616341a
PublicTrades <info@publictrades.com>
2023-10-10 17:56:15 -0400

    bg color updated

--- frontend/components/Profile.js
+++ frontend/components/Profile.js
@@ -198,7 +198,7 @@ export default function ProfilePg() {
                     <main className='mt-3'>
                         {/* Settings forms */}
                         <div id='personal' className="divide-y divide-white/5">
-                            <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
+                            <div className="grid max-w-7xl grid-cols-1 gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 bg-white lg:px-8">
                                 <div>
                                     <h2 className="text-base font-semibold leading-7 text-black">Personal Information</h2>
                                     <p className="mt-1 text-sm leading-6 text-slate-400">
