dc056b8ea6288c0534e00d44dee6370ffef185a2
PublicTrades <info@publictrades.com>
2023-10-11 19:00:22 -0400

    dashboard redo

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -268,45 +268,6 @@ const Dashboard = function ({ children }) {
                                                             Welcome {user?.displayName}
                                                         </h2>
                                                     </div>
-                                                    <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-1 lg:grid-cols-3">
-
-                                                        {cards.map((card) => (
-                                                            <div
-                                                                key={card.id}
-                                                                className="overflow-hidden rounded-lg bg-white shadow"
-                                                            >
-                                                                {/* <div className="p-5">
-                                                                    <div className="flex items-center">
-                                                                        <div className="flex-shrink-0"></div>
-                                                                        <div className="ml-5 w-0 flex-1">
-                                                                            <dl>
-                                                                                <dt className="truncate text-sm font-medium text-gray-500">
-                                                                                    {card.name}
-                                                                                </dt>
-                                                                                <dd>
-                                                                                    <div className="text-lg font-medium text-gray-900">
-                                                                                        {card.amount}
-                                                                                    </div>
-                                                                                </dd>
-                                                                            </dl>
-                                                                        </div>
-                                                                    </div>
-                                                                </div> */}
-                                                                <div className="bg-gray-50 px-5 py-3">
-                                                                    <div className="text-sm text-center">
-                                                                        <a
-                                                                            href={card.href}
-                                                                            className="font-medium text-gray-700 hover:text-gray-900"
-                                                                        >
-                                                                            <span>{card.name}</span>
-                                                                            <span>{card.amount}</span>
-
-                                                                        </a>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        ))}
-                                                    </div>
                                                 </div>
                                                 <div className="mt-5 flex justify-center sm:mt-0">
                                                     <a
@@ -316,6 +277,27 @@ const Dashboard = function ({ children }) {
                                                         Add Team Member
                                                     </a>
                                                 </div>
+                                            </div>
+                                            <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-1 lg:grid-cols-3">
+                                                {cards.map((card) => (
+                                                    <div
+                                                        key={card.id}
+                                                        className="overflow-hidden rounded-lg bg-white shadow"
+                                                    >
+                                                        <div className="bg-gray-50 px-5 py-3">
+                                                            <div className="text-sm text-center">
+                                                                <a
+                                                                    href={card.href}
+                                                                    className="font-medium text-gray-700 hover:text-gray-900"
+                                                                >
+                                                                    <span>{card.name}</span>
+                                                                    <span>{card.amount}</span>
+
+                                                                </a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                ))}
                                             </div>
                                         </div>
                                         {/* <div className="grid grid-cols-1 divide-y divide-gray-200 border-t border-gray-200 bg-gray-50 sm:grid-cols-1 sm:divide-x sm:divide-y-0">
