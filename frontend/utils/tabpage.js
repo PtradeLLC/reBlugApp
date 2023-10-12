5399d32e454f23797625b788c9ac81f925315d3a
PublicTrades <info@publictrades.com>
2023-10-12 13:19:23 -0400

    updates to the tabpage

--- frontend/utils/tabpage.js
+++ frontend/utils/tabpage.js
@@ -1,23 +1,17 @@
 const emailTab = `
-Email Conversational Tool is an AI-powered marketing tool that helps businesses improve their email communication by embedding a chatbot into their emails and newsletters. <br>
+Email Conversational Tool is an AI-powered marketing tool that helps businesses improve their email communication by embedding a chatbot into their emails and newsletters.\n
 This allows email recipients to interact with a knowledge-based chatbot that can answer their questions and provide support, help with fundraising, sales, marketing, and more.
 
-Here is an example of how Email Conversational Tool can be used: <br>
-A non-profit organization could use Email Conversational Tool to embed a chatbot into their donation email.<br> The chatbot could answer questions about the organization's mission and programs, and help donors make a donation.
+Here is an example of how Email Conversational Tool can be used: \n
+A non-profit organization could use Email Conversational Tool to embed a chatbot into their donation email.\n The chatbot could answer questions about the organization's mission and programs, and help donors make a donation.
 <br>
-An e-commerce company could use Email Conversational Tool to embed a chatbot into their product newsletter.<br> The chatbot could answer questions about the company's products, help customers place an order, and track their shipments.
+An e-commerce company could use Email Conversational Tool to embed a chatbot into their product newsletter.\n The chatbot could answer questions about the company's products, help customers place an order, and track their shipments.
 <br>
-A software company could use Email Conversational Tool to embed a chatbot into their customer support email.<br> The chatbot could answer questions about the company's software, help customers troubleshoot problems, and create support tickets.
+A software company could use Email Conversational Tool to embed a chatbot into their customer support email.\n The chatbot could answer questions about the company's software, help customers troubleshoot problems, and create support tickets.
 
 Email Conversational Tool is a powerful tool that can help businesses of all sizes improve their email communication and achieve their marketing goals.
 
-Here are some of the benefits of using Email Conversational Tool:<br>
-* Increased engagement: Email recipients are more likely to interact with emails that contain a chatbot.<br> This can lead to higher open rates, click-through rates, and conversions.
-* Improved customer service: Chatbots can provide 24/7 customer support, answering questions and resolving issues quickly and efficiently.
-* Increased sales: Chatbots can help customers find the products or services they're looking for, and guide them through the purchase process.
-* Reduced costs: Chatbots can automate many tasks that are currently handled by human customer service representatives, which can save businesses money.<br>
-<br>
-This tool is a valuable tool for any business that wants to improve its email communication and achieve its marketing goals.
+Features:\n
 `;
 
 const marketing_tab = ``;
