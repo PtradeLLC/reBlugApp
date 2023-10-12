e674d6edb23440ca2fbb38e33060763cf18974f1
PublicTrades <info@publictrades.com>
2023-10-12 13:40:30 -0400

    update to new line

--- frontend/utils/tabpage.js
+++ frontend/utils/tabpage.js
@@ -3,13 +3,13 @@ Email Conversational Tool is an AI-powered marketing tool that helps businesses 
 This allows email recipients to interact with a knowledge-based chatbot that can answer their questions and provide support, help with fundraising, sales, marketing, and more.
 \\
 Here is an example of how Email Conversational Tool can be used:\\
-\\● A non-profit organization could use Email Conversational Tool to embed a chatbot into their donation email.\\ The chatbot could answer questions about the organization's mission and programs, and help donors make a donation.
+\\ A non-profit organization could use Email Conversational Tool to embed a chatbot into their donation email.\\ The chatbot could answer questions about the organization's mission and programs, and help donors make a donation.
 
-\\● An e-commerce company could use Email Conversational Tool to embed a chatbot into their product newsletter.\\ The chatbot could answer questions about the company's products, help customers place an order, and track their shipments.
+\\ An e-commerce company could use Email Conversational Tool to embed a chatbot into their product newsletter.\\ The chatbot could answer questions about the company's products, help customers place an order, and track their shipments.
 
-\\● A software company could use Email Conversational Tool to embed a chatbot into their customer support email.\\ The chatbot could answer questions about the company's software, help customers troubleshoot problems, and create support tickets.
+\\ A software company could use Email Conversational Tool to embed a chatbot into their customer support email.\\ The chatbot could answer questions about the company's software, help customers troubleshoot problems, and create support tickets.
 
-\\● Email Conversational Tool is a powerful tool that can help businesses of all sizes improve their email communication and achieve their marketing goals.
+\\ Email Conversational Tool is a powerful tool that can help businesses of all sizes improve their email communication and achieve their marketing goals.
 
 Features:\\
 `;
