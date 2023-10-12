39c103afb4b13370f83ecc85f334dfc32bba0bd4
PublicTrades <info@publictrades.com>
2023-10-12 11:23:19 -0400

    event handler setup for tabs

--- frontend/components/Tabs.js
+++ frontend/components/Tabs.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 
 const tabs = [
-    { name: 'Features', href: 'features', current: true, content: 'Content for My Account tab' },
-    { name: 'Solution', href: 'solution', current: false, content: 'Content for Solution tab' },
+    { name: 'My Account', href: '#', current: true, content: 'Content for My Account tab' },
+    { name: 'Company', href: '#', current: false, content: 'Content for Company tab' },
     { name: 'Team Members', href: '#', current: false, content: 'Content for Team Members tab' },
     { name: 'Billing', href: '#', current: false, content: 'Content for Billing tab' },
 ];
@@ -11,8 +11,12 @@ function classNames(...classes) {
     return classes.filter(Boolean).join(' ');
 }
 
-export default function Tabs() {
+export default function Example() {
     const [selectedTab, setSelectedTab] = useState(tabs.find((tab) => tab.current));
+
+    const handleTabClick = (tab) => {
+        setSelectedTab(tab);
+    };
 
     return (
         <div>
@@ -23,7 +27,7 @@ export default function Tabs() {
                 <select
                     id="tabs"
                     name="tabs"
-                    className="block w-full rounded-md border-gray-300 focus:border-red-500 focus:ring-red-500"
+                    className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
                     value={selectedTab.name}
                     onChange={(e) => setSelectedTab(tabs.find((tab) => tab.name === e.target.value))}
                 >
@@ -46,14 +50,14 @@ export default function Tabs() {
                                 tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '',
                                 'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10'
                             )}
-                            onClick={() => setSelectedTab(tab)}
+                            onClick={() => handleTabClick(tab)}
                             aria-current={selectedTab === tab ? 'page' : undefined}
                         >
                             <span>{tab.name}</span>
                             <span
                                 aria-hidden="true"
                                 className={classNames(
-                                    selectedTab === tab ? 'bg-red-500' : 'bg-transparent',
+                                    selectedTab === tab ? 'bg-indigo-500' : 'bg-transparent',
                                     'absolute inset-x-0 bottom-0 h-0.5'
                                 )}
                             />
