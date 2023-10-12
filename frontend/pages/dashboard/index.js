c78de78fd329cf463b810b47e16238c5fcef597f
PublicTrades <info@publictrades.com>
2023-10-12 01:26:15 -0400

    bg5

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -286,9 +286,9 @@ const Dashboard = function ({ children }) {
                                                     <div
                                                         key={card.id}
                                                         // className={`overflow-hidden rounded-lg ${card.id === 1 ? 'bg-yellow-400' : card.id === 2 ? 'bg-blue-500' : card.id === 3 ? 'bg-blue-900' : null} shadow`}
-                                                        className={`overflow-hidden rounded-lg bg-gray-50 shadow ${card.id === 1 ? 'bg-yellow-400' : card.id === 2 ? 'bg-blue-500' : card.id === 3 ? 'bg-blue-900' : null} `}
+                                                        className={`overflow-hidden rounded-lg shadow ${card.id === 1 ? 'bg-yellow-400' : card.id === 2 ? 'bg-blue-500' : card.id === 3 ? 'bg-blue-900' : null} `}
                                                     >
-                                                        <div className={`bg-gray-50 px-5 py-3 ${card.id === 1 ? 'bg-yellow-400' : card.id === 2 ? 'bg-blue-500' : card.id === 3 ? 'bg-blue-900' : null}`}>
+                                                        <div className={`px-5 py-3 ${card.id === 1 ? 'bg-yellow-400' : card.id === 2 ? 'bg-blue-500' : card.id === 3 ? 'bg-blue-900' : null}`}>
                                                             <div className="text-sm text-center">
                                                                 <Link
                                                                     href={card.href}
