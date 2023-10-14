7f71dffe064719402e8db2a2b818fe0bf60ee430
PublicTrades <info@publictrades.com>
2023-10-14 17:13:48 -0400

    updated kpi again

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -127,117 +127,6 @@ const Dashboard = function ({ children }) {
     const [selectedKpi, setSelectedKpi] = useState("blah blah blah");
     const [loading, setLoading] = useState(false);
 
-    // const koi = (title) => {
-    //     if (title === "Email Conversational") {
-    //         emailAction.map((action) => (
-    //             <div key={action.id} className="">
-    //                 <div className="mt-8 gap-4">
-    //                     <h3 className="text-lg font-medium">
-    //                         <Link
-    //                             href={action.href}
-    //                             className="focus:outline-none"
-    //                         >
-
-    //                             {/* <span
-    //                                 className="absolute inset-0"
-    //                                 aria-hidden="true"
-    //                             /> */}
-    //                             {action.name}
-    //                         </Link>
-    //                     </h3>
-    //                     <p className="mt-2 text-sm text-gray-500">
-    //                         Doloribus dolores nostrum quia qui natus officia
-    //                         quod et dolorem. Sit repellendus qui ut at
-    //                         blanditiis et quo et molestiae.
-    //                     </p>
-    //                 </div>
-    //                 <span
-    //                     className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
-    //                     aria-hidden="true"
-    //                 >
-    //                     <svg
-    //                         className="h-6 w-6"
-    //                         fill="currentColor"
-    //                         viewBox="0 0 24 24"
-    //                     >
-    //                         <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-    //                     </svg>
-    //                 </span>
-    //             </div>
-    //         ))
-    //     } else if (title === "Campaign Automation") {
-    //         automationAction.map((action) => (
-    //             <div key={action.id} className="">
-    //                 <div className="mt-8 gap-4">
-    //                     <h3 className="text-lg font-medium">
-    //                         <Link href={action.href} className="focus:outline-none">
-    //                             {/* <span
-    //                                 className="absolute inset-0"
-    //                                 aria-hidden="true"
-    //                             /> */}
-    //                             {action.name}
-    //                         </Link>
-    //                     </h3>
-    //                     <p className="mt-2 text-sm text-gray-500">
-    //                         Doloribus dolores nostrum quia qui natus officia
-    //                         quod et dolorem. Sit repellendus qui ut at
-    //                         blanditiis et quo et molestiae.
-    //                     </p>
-    //                 </div>
-    //                 <span
-    //                     className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
-    //                     aria-hidden="true"
-    //                 >
-    //                     <svg
-    //                         className="h-6 w-6"
-    //                         fill="currentColor"
-    //                         viewBox="0 0 24 24"
-    //                     >
-    //                         <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-    //                     </svg>
-    //                 </span>
-    //             </div>
-    //         ))
-    //     } else if (title === "Messaging Platform") {
-    //         marketingAction.map((action, actionIdx) => (
-    //             <div key={action.id} className="">
-    //                 <div className="mt-8 gap-4">
-    //                     <h3 className="text-lg font-medium">
-    //                         <Link href={action.href} className="focus:outline-none">
-
-    //                             {/* <span
-    //                                 className="absolute inset-0"
-    //                                 aria-hidden="true"
-    //                             /> */}
-    //                             {action.name}
-    //                         </Link>
-    //                     </h3>
-    //                     <p className="mt-2 text-sm text-gray-500">
-    //                         Doloribus dolores nostrum quia qui natus officia
-    //                         quod et dolorem. Sit repellendus qui ut at
-    //                         blanditiis et quo et molestiae.
-    //                     </p>
-    //                 </div>
-    //                 <span
-    //                     className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
-    //                     aria-hidden="true"
-    //                 >
-    //                     <svg
-    //                         className="h-6 w-6"
-    //                         fill="currentColor"
-    //                         viewBox="0 0 24 24"
-    //                     >
-    //                         <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-    //                     </svg>
-    //                 </span>
-    //             </div>
-    //         ))
-
-    //     } else {
-    //         return null;
-    //     }
-    // };
-
     const koi = (title) => {
         if (title === "Email Conversational") {
             return emailAction.map((action) => (
@@ -260,7 +149,7 @@ const Dashboard = function ({ children }) {
                 </div>
             ))
         } else if (title === "Messaging Platform") {
-            return automationAction.map((action) => (
+            return marketingAction.map((action) => (
                 <div key={action.id} className="...">
                     <h3 className="text-lg font-medium">
                         <Link href="" className="focus:outline-none">
