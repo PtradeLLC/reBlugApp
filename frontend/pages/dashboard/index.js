28d8a0b0e759c29879005a5403dbda28a9e4a42a
PublicTrades <info@publictrades.com>
2023-10-14 14:55:02 -0400

    kpi stubborn

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -490,6 +490,7 @@ const Dashboard = function ({ children }) {
                                         {/* If selectedKpi exist, then component is rendered */}
                                         {selectedKpi && (
                                             <div className="">
+                                                {console.log("SELECTEDKPI:", selectedKpi)}
                                                 <Kpi selectedKpi={selectedKpi} />
                                                 {/* {kpi(selectedKpi)} */}
                                             </div>
