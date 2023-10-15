0fc6ef56a8b822f8581949b0e2e4f5a5a7539f17
PublicTrades <info@publictrades.com>
2023-10-15 17:37:42 -0400

    updated kpi on dash

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -129,18 +129,23 @@ const Dashboard = function ({ children }) {
 
     const kpi = (title) => {
         if (title === "Email Conversational") {
-            return emailAction.map((action) => (
-                <div key={action.id} className="bg-red-500">
-                    <h3 className="text-lg font-medium">
-                        <Link href="" className="focus:outline-none">
-                            {action.name}
-                        </Link>
-                    </h3>
+            return (
+                <div>
+                    <div></div>
                     <div>
-                        <p>This contains charts</p>
+                        {emailAction.map((action) => (
+                            <div key={action.id} className="bg-red-500 grid">
+                                <h3 className="text-lg font-medium">
+                                    <Link href="" className="focus:outline-none">
+                                        {action.name}
+                                    </Link>
+                                </h3>
+                            </div>
+                        ))};
                     </div>
+
                 </div>
-            ))
+            )
         } else if (title === "Campaign Automation") {
             return automationAction.map((action) => (
                 <div key={action.id} className="bg-red-100 grid">
