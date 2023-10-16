17e4c0019872c2dfc42c6965f5a1f029a5248cf3
PublicTrades <info@publictrades.com>
2023-10-16 17:14:20 -0400

    updted features dash

--- frontend/utils/tabpage.js
+++ frontend/utils/tabpage.js
@@ -2,59 +2,14 @@ import { Grid, Flex, Text } from '@radix-ui/themes'
 
 
 const emailTab = `
-${<Grid align="center" columns="2" gap="5" p="3">
-        <Flex direction="column">
-            <Text size="3" weight="bold">
-                Get started
-            </Text>
-            <Text color="gray" size="2">
-                Start your next project in minutes
-            </Text>
-        </Flex>
-
-        <Flex direction="column">
-            <Text size="2" weight="bold">
-                Get started
-            </Text>
-            <Text color="gray" size="2">
-                Start your next project in minutes
-            </Text>
-        </Flex>
-
-        <Flex direction="column">
-            <Text size="2" weight="bold">
-                Get started
-            </Text>
-            <Text color="gray" size="1">
-                Start your next project in minutes
-            </Text>
-        </Flex>
-
-        <Flex direction="column">
-            <Text size="1" weight="bold">
-                Get started
-            </Text>
-            <Text color="gray" size="1">
-                Start your next project in minutes
-            </Text>
-        </Flex>
-    </Grid>
-    }
 Features:
 1. Phone - Use this tool to make sales calls in your native language, and let AI feature translate the call to your prospective client's native language.
 2. Survey - Ditch conventional surveys forms and create AI-powered surveys through conversation. AI-powered surveys are more engaging and efficient, and they can help you collect richer data. 
 3. Chatbot - Answer questions, provide information, customer support using AI powered conversational chatbot in emails / newsletters.
 4. eCommerce - Have chatbot double up as your sales agent by helping prospective customers find the products or services they're looking for, and guide them through the purchase process with this tool.
 5. Reporting - Collect rich insight on your campaign, what is working, and what's not, and how you can improve to reach your goals quicker through the outcomes tab.
-
-Email Conversational Tool is an AI-powered marketing tool that helps businesses improve their email communication by embedding a chatbot into their emails and newsletters.
-This allows email recipients to interact with a knowledge-based chatbot that can answer their questions and provide support, help with fundraising, sales, marketing, and more.
-Here is an example of how Email Conversational Tool can be used:
-A non-profit organization could use Email Conversational Tool to embed a chatbot into their donation email. The chatbot could answer questions about the organization's mission and programs, and help donors make a donation.
-An e-commerce company could use Email Conversational Tool to embed a chatbot into their product newsletter. The chatbot could answer questions about the company's products, help customers place an order, and track their shipments.
-A Creator could use this tool to connect with audience, answer questions, and to promote products or service.
-A software company could use Email Conversational Tool to embed a chatbot into their customer support email. The chatbot could answer questions about the company's software, help customers troubleshoot problems, and create support tickets.
-Email Conversational Tool is a powerful tool that can help businesses of all sizes improve their email communication and achieve their marketing goals.
+6. SMS - ...
+7. NetRadar - ...
 `;
 
 const marketing_tab = `There are no campaigns to track. Please launch a campaign first.`;
@@ -62,3 +17,13 @@ const marketing_tab = `There are no campaigns to track. Please launch a campaign
 const outcomes_tab = `There is nothing to report yet. Please launch a campaign first.`;
 
 export { emailTab, marketing_tab, outcomes_tab }
+
+
+// Email Conversational Tool is an AI-powered marketing tool that helps businesses improve their email communication by embedding a chatbot into their emails and newsletters.
+// This allows email recipients to interact with a knowledge-based chatbot that can answer their questions and provide support, help with fundraising, sales, marketing, and more.
+// Here is an example of how Email Conversational Tool can be used:
+// A non-profit organization could use Email Conversational Tool to embed a chatbot into their donation email. The chatbot could answer questions about the organization's mission and programs, and help donors make a donation.
+// An e-commerce company could use Email Conversational Tool to embed a chatbot into their product newsletter. The chatbot could answer questions about the company's products, help customers place an order, and track their shipments.
+// A Creator could use this tool to connect with audience, answer questions, and to promote products or service.
+// A software company could use Email Conversational Tool to embed a chatbot into their customer support email. The chatbot could answer questions about the company's software, help customers troubleshoot problems, and create support tickets.
+// Email Conversational Tool is a powerful tool that can help businesses of all sizes improve their email communication and achieve their marketing goals.
