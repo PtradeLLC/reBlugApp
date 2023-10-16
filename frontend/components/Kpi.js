080530927b41b52d94d775c44092289f0360cda1
PublicTrades <info@publictrades.com>
2023-10-16 13:32:06 -0400

    updated legend alignment

--- frontend/components/Kpi.js
+++ frontend/components/Kpi.js
@@ -110,32 +110,30 @@ const EmailBarChart = ({ name }) => {
             )}
             {name === "Delivered" && (
                 <ResponsiveContainer width="100%" height={200}>
-                    <span className='flex justify-between'>
-                        <RadialBarChart
-                            innerRadius="10%"
-                            outerRadius="80%"
-                            data={delivered_data}
-                            startAngle={180}
-                            endAngle={0}
-                        >
-                            <RadialBar
-                                minAngle={15}
-                                label={{ fill: '#666', position: 'insideStart' }}
-                                background
-                                clockWise
-                                dataKey='uv'
-                            />
-                            <Legend
-                                iconSize={10}
-                                width={120}
-                                height={140}
-                                layout='vertical'
-                                verticalAlign='middle'
-                                align="right"
-                            />
-                            <Tooltip />
-                        </RadialBarChart>
-                    </span>
+                    <RadialBarChart
+                        innerRadius="10%"
+                        outerRadius="80%"
+                        data={delivered_data}
+                        startAngle={180}
+                        endAngle={0}
+                    >
+                        <RadialBar
+                            minAngle={15}
+                            label={{ fill: '#666', position: 'insideStart' }}
+                            background
+                            clockWise
+                            dataKey='uv'
+                        />
+                        <Legend
+                            iconSize={10}
+                            width={120}
+                            height={140}
+                            layout='horizontal'
+                            verticalAlign='top'
+                            align="center"
+                        />
+                        <Tooltip />
+                    </RadialBarChart>
                 </ResponsiveContainer>
             )}
         </>
