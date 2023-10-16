aadb36fbd7ee34f9e4ac1d202e8bd17c4ee36a04
PublicTrades <info@publictrades.com>
2023-10-15 22:17:44 -0400

    v2 review

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -397,10 +397,8 @@ const Dashboard = function ({ children }) {
                                             <h2 className="sr-only" id="quick-links-title">
                                                 Quick links
                                             </h2>
-                                            {selectedComponent ? kpi(selectedComponent) : <p>While you were gone</p>}
                                             {emailAction.map((action) => (
                                                 <section key={action.id} className="mt-4">
-                                                    {/* {selectedComponent ? kpi(selectedComponent) : <p>While you were gone</p>} */}
                                                     {selectedKpi && (
                                                         <div
                                                             className={classNames(
