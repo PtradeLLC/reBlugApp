04d67f7e1bee492678ad960e10843abf968ab8d1
PublicTrades <info@publictrades.com>
2023-10-16 02:38:46 -0400

    updated label

--- frontend/components/Kpi.js
+++ frontend/components/Kpi.js
@@ -4,38 +4,38 @@ import { BarChart, Bar, Area, linearGradient, XAxis, YAxis, CartesianGrid, Toolt
 const data = [
     {
         "name": "Mon",
-        "delivered": 4000,
-        "pv": 2400
+        "opened": 4000,
+        "delivered": 2400
     },
     {
         "name": "Tue",
-        "delivered": 3000,
-        "pv": 1398
+        "opened": 3000,
+        "delivered": 1398
     },
     {
         "name": "Weds",
-        "delivered": 2000,
-        "pv": 9800
+        "opened": 2000,
+        "delivered": 9800
     },
     {
         "name": "Thurs",
-        "delivered": 2780,
-        "pv": 3908
+        "opened": 2780,
+        "delivered": 3908
     },
     {
         "name": "Fri",
-        "delivered": 1890,
-        "pv": 4800
+        "opened": 1890,
+        "delivered": 4800
     },
     {
         "name": "Sat",
-        "delivered": 1890,
-        "pv": 4800
+        "opened": 1890,
+        "delivered": 4800
     },
     {
         "name": "Sun",
-        "delivered": 1890,
-        "pv": 4800
+        "opened": 1890,
+        "delivered": 4800
     },
 ];
 
@@ -44,11 +44,11 @@ const EmailBarChart = () => {
         <AreaChart width={500} height={200} data={data}
             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
             <defs>
-                <linearGradient id="colordelivered" x1="0" y1="0" x2="0" y2="1">
+                <linearGradient id="coloropened" x1="0" y1="0" x2="0" y2="1">
                     <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
                     <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
                 </linearGradient>
-                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <linearGradient id="colordelivered" x1="0" y1="0" x2="0" y2="1">
                     <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
                     <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
                 </linearGradient>
@@ -57,8 +57,8 @@ const EmailBarChart = () => {
             <YAxis />
             <CartesianGrid strokeDasharray="3 3" />
             <Tooltip />
-            <Area type="monotone" dataKey="delivered" stroke="#EF6262" fillOpacity={1} fill="#F8485E" />
-            <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+            <Area type="monotone" dataKey="opened" stroke="#EF6262" fillOpacity={1} fill="#F8485E" />
+            <Area type="monotone" dataKey="delivered" stroke="#82ca9d" fillOpacity={1} fill="url(#colordelivered)" />
         </AreaChart>
     );
 }
