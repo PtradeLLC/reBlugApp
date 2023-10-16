7a951bc1569f82f7395176d88ccc6480f1c021d2
PublicTrades <info@publictrades.com>
2023-10-16 12:25:08 -0400

    updated chart on emailAction

--- frontend/components/Kpi.js
+++ frontend/components/Kpi.js
@@ -109,19 +109,32 @@ const EmailBarChart = ({ name }) => {
                 </AreaChart>
             )}
             {name === "Delivered" && (
-                <RadialBarChart
-                    width={730}
-                    height={250}
-                    innerRadius="10%"
-                    outerRadius="80%"
-                    data={delivered_data}
-                    startAngle={180}
-                    endAngle={0}
-                >
-                    <RadialBar minAngle={15} label={{ fill: '#666', position: 'insideStart' }} background clockWise={true} dataKey='uv' />
-                    <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' align="right" />
-                    <Tooltip />
-                </RadialBarChart>
+                <ResponsiveContainer width="100%" height={400}>
+                    <RadialBarChart
+                        innerRadius="10%"
+                        outerRadius="80%"
+                        data={delivered_data}
+                        startAngle={180}
+                        endAngle={0}
+                    >
+                        <RadialBar
+                            minAngle={15}
+                            label={{ fill: '#666', position: 'insideStart' }}
+                            background
+                            clockWise
+                            dataKey='uv'
+                        />
+                        <Legend
+                            iconSize={10}
+                            width={120}
+                            height={140}
+                            layout='vertical'
+                            verticalAlign='middle'
+                            align="right"
+                        />
+                        <Tooltip />
+                    </RadialBarChart>
+                </ResponsiveContainer>
             )}
         </>
     );
