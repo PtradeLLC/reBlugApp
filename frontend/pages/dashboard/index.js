650fcd460fcc5e1ed220457f3c64c4e68b55c8da
PublicTrades <info@publictrades.com>
2023-10-16 14:36:42 -0400

    delievered updated

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -182,7 +182,7 @@ const Dashboard = function ({ children }) {
                         </Link>
                     </h3>
                     <div>
-                        {action.name === "Processed" && (
+                        {action.name === "Delivered" && (
                             <span className="w-full">
                                 <Kpi name={action.name} />
                             </span>
