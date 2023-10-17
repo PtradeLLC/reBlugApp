3ab0fdaeda36e9b7e8944efc3d94b3ff84613846
PublicTrades <info@publictrades.com>
2023-10-17 14:25:45 -0400

    updated summar v2

--- frontend/components/CampaignSummary.js
+++ frontend/components/CampaignSummary.js
@@ -6,9 +6,9 @@ const CampaignSummary = () => {
 
     const [summary, setSummary] = useState("");
     return (
-        <div className="divide-y mt-4 divide-gray-200 overflow-hidden rounded-lg bg-white shadow sm:grid sm:grid-cols-1 lg:gap-4 sm:gap-px sm:divide-y-0">
+        <div className="divide-y mt-4 divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
             <div>
-                <span className="bg-white sm:col-span-1 justify-center items-center text-center py-2 font-semibold text-xl">Campaign Summary</span>
+                <span className="bg-white justify-center items-center text-center py-2 font-semibold text-xl">Campaign Summary</span>
             </div>
             {summary ? <div>Here are your summary</div> :
                 <div>
