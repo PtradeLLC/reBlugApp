5e45dc557f95c9269e2e908fc69fd397792f307c
PublicTrades <info@publictrades.com>
2023-10-17 16:00:28 -0400

    class updated

--- frontend/pages/dashboard/index.js
+++ frontend/pages/dashboard/index.js
@@ -428,7 +428,7 @@ const Dashboard = function ({ children }) {
                                         </section>
                                         <section className={`mt-4 ${selectedComponent === "Campaign Automation" || selectedComponent === "Messaging Platform" ? "blur-sm" : ""}`}>
                                             {selectedKpi && (
-                                                <div className="divide-y mt-4 divide-gray-200 overflow-hidden rounded-lg bg-white shadow sm:grid sm:grid-cols-3 lg:gap-4 sm:gap-px sm:divide-y-0">
+                                                <div className={`${selectedComponent ? `divide-y mt-4 divide-gray-200 overflow-hidden rounded-lg bg-white shadow sm:grid sm:grid-cols-3 lg:gap-4 sm:gap-px sm:divide-y-0` : ""}`}>
                                                     <h2 className="sr-only" id="quick-links-title">
                                                         Summary
                                                     </h2>
