cd20d1f2f01bf2c7e3f051cba294cbb0a9feabe7
PublicTrades <info@publictrades.com>
2023-10-21 15:43:37 -0400

    updated body

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-no-repeat h-screen">
+      <body className="bg-no-repeat w-full bg-white">
         <Main />
         <NextScript />
       </body>
