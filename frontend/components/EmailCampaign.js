4f3ec6cc3f8c2379f076fada2638844aa0d4a14f
PublicTrades <info@publictrades.com>
2023-10-25 13:28:59 -0400

    edit side window v1

--- frontend/components/EmailCampaign.js
+++ frontend/components/EmailCampaign.js
@@ -38,7 +38,7 @@ export default function EmailCamp() {
                         </div>
                     </div>
                 </div>
-                <div className="ml-4 mt-4 flex flex-shrink-0">
+                <div className="ml-5 mt-4 flex flex-shrink-0">
                     <button
                         type="button"
                         className="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
