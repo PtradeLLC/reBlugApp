6f3fb3814df25622171901b9711c3af543c53ec7
PublicTrades <info@publictrades.com>
2023-10-26 13:23:06 -0400

    test survey v7

--- frontend/components/EmailCampaign.js
+++ frontend/components/EmailCampaign.js
@@ -71,9 +71,11 @@ export default function EmailCamp() {
                         <span>Newsletter</span>
                     </button>
                 </div>
-                <div className="inline-block w-full p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl">
-                    {survey && <EmailForm email={email} survey={survey} newsletter={newsletter} />}
-                </div>
+                {survey && (
+                    <div className="inline-block w-full p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-xl">
+                        <EmailForm email={email} survey={survey} newsletter={newsletter} />
+                    </div>
+                )}
             </div>
         </div>
     )
