a46a30caf7c9e340eba8765655ef96d916436f7d
PublicTrades <info@publictrades.com>
2023-10-27 17:08:38 -0400

    ApprovedCheckPoint: Open Modal

--- frontend/components/EmailCampaign.js
+++ frontend/components/EmailCampaign.js
@@ -44,19 +44,19 @@ export default function EmailCamp({ openModal, setOpenModal }) {
                 <div className="ml-5 mt-4 flex flex-shrink-0">
                     <button
                         type="button"
+                        onClick={() => onButtonClick('email')}
+                        className="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    >
+                        <EnvelopeIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                        <span>Product Email</span>
+                    </button>
+                    <button
+                        type="button"
                         onClick={() => onButtonClick('survey')}
                         className="relative inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                     >
                         <PencilSquareIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
                         <span>Survey</span>
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => onButtonClick('email')}
-                        className="relative ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                    >
-                        <EnvelopeIcon className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                        <span>Email</span>
                     </button>
                     <button
                         type="button"
@@ -68,8 +68,7 @@ export default function EmailCamp({ openModal, setOpenModal }) {
                     </button>
                 </div>
             </div>
-
-            <div>
+            <div className='mt-3'>
                 <EmailForm />
             </div>
         </div>
