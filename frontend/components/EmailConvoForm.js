c20be793ddcd547597b2a3c1a23fe1d7771c91b0
PublicTrades <info@publictrades.com>
2023-10-27 18:34:31 -0400

    centered div V2

--- frontend/components/EmailConvoForm.js
+++ frontend/components/EmailConvoForm.js
@@ -9,122 +9,124 @@ export default function EmailForm({ campaignEmail }) {
         <>
             <form>
                 <div className="space-y-12 mt-2">
-                    <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
-                        {campaignEmail ? null : (
-                            <div className="flex justify-center items-center h-screen">
-                                <div className="email-camp">
-                                    <h2 className="text-base font-semibold leading-7 text-gray-900">Email Campaign</h2>
-                                    <p className="mt-1 text-sm leading-6 text-gray-600">
-                                        From above, select an action to perform.
-                                    </p>
+                    <div className="relative">
+                        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+                            {campaignEmail ? null : (
+                                <div className="flex justify-center items-center h-screen">
+                                    <div className="email-camp absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                                        <h2 className="text-base font-semibold leading-7 text-gray-900">Email Campaign</h2>
+                                        <p className="mt-1 text-sm leading-6 text-gray-600">
+                                            From above, select an action to perform.
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
-                        )}
+                            )}
 
-                        <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
-                            {campaignEmail === "email" && (
-                                <>
-                                    <div className="sm:col-span-4">
-                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                            Email address
-                                        </label>
-                                        <div className="mt-2">
-                                            <input
-                                                id="email"
-                                                name="email"
-                                                type="email"
-                                                autoComplete="email"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
-                                            />
-                                        </div>
-                                    </div>
-                                    <div className="col-span-full">
-                                        <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                                            About
-                                        </label>
-                                        <div className="mt-2">
-                                            <textarea
-                                                id="about"
-                                                name="about"
-                                                rows={3}
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
-                                                defaultValue={''}
-                                            />
-                                        </div>
-                                        <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-                                    </div>
-                                    <div className="col-span-full">
-                                        <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
-                                            Photo
-                                        </label>
-                                        <div className="mt-2 flex items-center gap-x-3">
-                                            <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
-                                            <button
-                                                type="button"
-                                                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                            >
-                                                Change
-                                            </button>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-span-full">
-                                        <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                                            Cover photo
-                                        </label>
-                                        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                                            <div className="text-center">
-                                                <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                                                <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                                                    <label
-                                                        htmlFor="file-upload"
-                                                        className="relative cursor-pointer rounded-md bg-white font-semibold text-red-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-red-600 focus-within:ring-offset-2 hover:text-red-500"
-                                                    >
-                                                        <span>Upload a file</span>
-                                                        <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                                                    </label>
-                                                    <p className="pl-1">or drag and drop</p>
-                                                </div>
-                                                <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                            <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+                                {campaignEmail === "email" && (
+                                    <>
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Email address
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="email"
+                                                    name="email"
+                                                    type="email"
+                                                    autoComplete="email"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                                                />
                                             </div>
                                         </div>
-                                    </div>
-                                </>
-                            )}
+                                        <div className="col-span-full">
+                                            <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                                                About
+                                            </label>
+                                            <div className="mt-2">
+                                                <textarea
+                                                    id="about"
+                                                    name="about"
+                                                    rows={3}
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                                                    defaultValue={''}
+                                                />
+                                            </div>
+                                            <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+                                        </div>
+                                        <div className="col-span-full">
+                                            <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Photo
+                                            </label>
+                                            <div className="mt-2 flex items-center gap-x-3">
+                                                <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
+                                                <button
+                                                    type="button"
+                                                    className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                                                >
+                                                    Change
+                                                </button>
+                                            </div>
+                                        </div>
 
-                            {campaignEmail === "newsletter" && (
-                                <>
-                                    <div className="sm:col-span-4">
-                                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                                            Email address
-                                        </label>
-                                        <div className="mt-2">
-                                            <input
-                                                id="email"
-                                                name="email"
-                                                type="email"
-                                                autoComplete="email"
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
-                                            />
+                                        <div className="col-span-full">
+                                            <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Cover photo
+                                            </label>
+                                            <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                                                <div className="text-center">
+                                                    <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                                                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                                                        <label
+                                                            htmlFor="file-upload"
+                                                            className="relative cursor-pointer rounded-md bg-white font-semibold text-red-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-red-600 focus-within:ring-offset-2 hover:text-red-500"
+                                                        >
+                                                            <span>Upload a file</span>
+                                                            <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                                                        </label>
+                                                        <p className="pl-1">or drag and drop</p>
+                                                    </div>
+                                                    <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-span-full">
-                                        <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                                            About
-                                        </label>
-                                        <div className="mt-2">
-                                            <textarea
-                                                id="about"
-                                                name="about"
-                                                rows={3}
-                                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
-                                                defaultValue={''}
-                                            />
+                                    </>
+                                )}
+
+                                {campaignEmail === "newsletter" && (
+                                    <>
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Email address
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="email"
+                                                    name="email"
+                                                    type="email"
+                                                    autoComplete="email"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
                                         </div>
-                                        <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-                                    </div>
-                                </>
-                            )}
+                                        <div className="col-span-full">
+                                            <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                                                About
+                                            </label>
+                                            <div className="mt-2">
+                                                <textarea
+                                                    id="about"
+                                                    name="about"
+                                                    rows={3}
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                                                    defaultValue={''}
+                                                />
+                                            </div>
+                                            <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+                                        </div>
+                                    </>
+                                )}
+                            </div>
                         </div>
                     </div>
 
