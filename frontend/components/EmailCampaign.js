fcf2fdaf4158c822f455842480d09cb2fab52061
PublicTrades <info@publictrades.com>
2023-10-28 17:29:45 -0400

    updated

--- frontend/components/EmailCampaign.js
+++ frontend/components/EmailCampaign.js
@@ -31,12 +31,12 @@ export default function EmailCamp({ openModal, setOpenModal }) {
                             </div>
                         </div>
                     </div>
-                    <div className="ml-6 mt-4 flex flex-shrink-0">
-                        <div className="flex justify-center mb-2 items-center">
-                            <div className="email-camp">
-                                <h2 className="text-base font-semibold leading-7 text-gray-900">Email Campaign</h2>
-                            </div>
+                    <div className="flex justify-center mb-2 items-center">
+                        <div className="email-camp">
+                            <h2 className="text-base font-semibold leading-7 text-gray-900">Email Campaign</h2>
                         </div>
+                    </div>
+                    <div className="ml-6 mt-4 flex flex-shrink-0">
                         <button
                             type="button"
                             onClick={() => onButtonClick('email')}
