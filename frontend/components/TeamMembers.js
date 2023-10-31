b52b0f87b9d919c60d1f58eeea950dfb449fbb67
PublicTrades <info@publictrades.com>
2023-10-31 16:09:52 -0400

    updated form ele

--- frontend/components/TeamMembers.js
+++ frontend/components/TeamMembers.js
@@ -76,28 +76,37 @@ export default function Team({ show, setShow }) {
                                     </div>
                                 </div>
                                 <div className="mt-5 sm:mt-6">
-                                    {emailSent ? (
-                                        <>
-                                            <p className="text-sm text-gray-500">{emailMessage}</p>
+                                    {emailSent ? emailMessage : <form onSubmit={handleSubmit}>
+
+                                        <label htmlFor="team" className="block text-sm font-medium leading-6 text-gray-900">
+                                            Member's email
+                                        </label>
+                                        <input
+                                            type="text"
+                                            name="team"
+                                            id="team"
+                                            value={email}
+                                            onChange={handleChange}
+                                            required
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                                            placeholder="email@example.com"
+                                            aria-describedby="team-description"
+                                        />
+                                        {emailSent ?
                                             <button
                                                 type="button"
-                                                onClick={() => setShow(false)} // Added onClick handler
+                                                onClick={(() => { setShow(false) })}
                                                 className="inline-flex mt-3 w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                                             >
                                                 Close
-                                            </button>
-                                        </>
-                                    ) : (
-                                        <form onSubmit={handleSubmit}>
-                                            {/* Rest of the form code */}
+                                            </button> :
                                             <button
                                                 type="button"
                                                 className="inline-flex mt-3 w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                                             >
                                                 Add Member
-                                            </button>
-                                        </form>
-                                    )}
+                                            </button>}
+                                    </form>}
                                 </div>
                             </Dialog.Panel>
                         </Transition.Child>
