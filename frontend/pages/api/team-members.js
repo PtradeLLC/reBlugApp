bf34a0d877ce61347ab97ea8cd92d242194205de
PublicTrades <info@publictrades.com>
2023-10-31 17:25:24 -0400

    updated api route

--- frontend/pages/api/team-members.js
+++ frontend/pages/api/team-members.js
@@ -4,15 +4,34 @@ import { withAccelerate } from '@prisma/extension-accelerate'
 const prisma = new PrismaClient().$extends(withAccelerate())
 
 export default async function handler(req, res) {
-    const { data } = req.body;
     try {
-        if (req.method === "POST") {
-            console.log("from backend", data);
-        } else {
-            console.log("There is an issue");
+        // Check if the request method is POST
+        if (req.method !== "POST") {
+            throw new Error('Invalid HTTP method');
         }
-        res.status(200).json({ name: 'John Doe' });
+
+        // Extract the email from the request body
+        const { email } = req.body;
+
+        // Check if email is provided
+        if (!email) {
+            return res.status(400).json({ message: "No email provided" });
+        }
+
+        // Create a new record in the emailList table
+        const emailData = await prisma.emailList.create({
+            data: {
+                email: email
+            }
+        });
+
+        // Extract the created email
+        const createdEmail = emailData.email;
+
+        // Respond with the created email
+        res.status(200).json({ createdEmail });
     } catch (error) {
-        res.status(500).json({ message: `There is an error ${error}` })
+        // Handle any potential errors
+        res.status(500).json({ message: `Internal Server Error: ${error.message}` });
     }
 }
