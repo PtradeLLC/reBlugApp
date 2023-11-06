6afa27266be17d4c1f8e7ec82eb14ecc59bf6695
PublicTrades <info@publictrades.com>
2023-11-06 14:03:35 -0500

    prisma debug

--- frontend/pages/api/team-members.js
+++ frontend/pages/api/team-members.js
@@ -1,7 +1,17 @@
+import { Pool, neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import dotenv from 'dotenv'
+import ws from 'ws'
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+dotenv.config()
+neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
+
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool)
+
+const prisma = new PrismaClient({ adapter })
 
 export default async function handler(req, res) {
     // Check if the request method is POST
