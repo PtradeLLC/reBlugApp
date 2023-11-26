9a9f28f84298a701e649d196624db811c7049453
PublicTrades <info@publictrades.com>
2023-11-26 13:02:59 -0500

    redirect fix one

--- frontend/pages/api/verify/[token].js
+++ frontend/pages/api/verify/[token].js
@@ -1,4 +1,3 @@
-// Example verification API route
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
@@ -35,7 +34,7 @@ export default async function handler(req, res) {
             });
 
             if (!user) {
-                console.error("There has been an error:", error);
+                console.error("There has been an error: Token is invalid or expired");
                 throw new Error('Token is invalid or expired');
             }
 
@@ -58,8 +57,9 @@ export default async function handler(req, res) {
                 },
             });
 
-            // Redirect immediately using res.redirect
-            res.redirect('/api/auth/signin');
+            // Redirect immediately
+            res.writeHead(302, { Location: '/api/auth/signin' });
+            res.end();
         } catch (error) {
             console.error(error);
             return res.status(500).json({ error: `An error occurred during verification: ${error.message}` });
@@ -70,3 +70,79 @@ export default async function handler(req, res) {
 
     return res.status(404).json({ message: "Not Found" });
 }
+
+
+
+
+// // Example verification API route
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default async function handler(req, res) {
+//     if (req.method === "GET") {
+//         const { token } = req.query;
+
+//         if (!token) {
+//             return res.status(400).json({ message: "Token is missing." });
+//         }
+
+//         try {
+//             const user = await prisma.user.findFirst({
+//                 where: {
+//                     VerificationTokens: {
+//                         some: {
+//                             AND: [
+//                                 {
+//                                     activatedAt: null,
+//                                 },
+//                                 {
+//                                     createdAt: {
+//                                         gt: new Date(Date.now() - 24 * 60 * 60 * 1000), // 24 hours ago
+//                                     },
+//                                 },
+//                                 {
+//                                     token,
+//                                 },
+//                             ],
+//                         },
+//                     },
+//                 },
+//             });
+
+//             if (!user) {
+//                 console.error("There has been an error:", error);
+//                 throw new Error('Token is invalid or expired');
+//             }
+
+//             // Update user status to verified
+//             await prisma.user.update({
+//                 where: {
+//                     id: user.id,
+//                 },
+//                 data: {
+//                     isVerified: true,
+//                 },
+//             });
+
+//             await prisma.verificationToken.update({
+//                 where: {
+//                     token,
+//                 },
+//                 data: {
+//                     activatedAt: new Date(),
+//                 },
+//             });
+
+//             // Redirect immediately using res.redirect
+//             res.redirect('/api/auth/signin');
+//         } catch (error) {
+//             console.error(error);
+//             return res.status(500).json({ error: `An error occurred during verification: ${error.message}` });
+//         } finally {
+//             await prisma.$disconnect();
+//         }
+//     }
+
+//     return res.status(404).json({ message: "Not Found" });
+// }
