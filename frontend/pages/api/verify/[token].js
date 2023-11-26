983d8296cec6d9710abcaefa80f9b2721405ffa4
PublicTrades <info@publictrades.com>
2023-11-26 13:57:25 -0500

    fix for verify

--- frontend/pages/api/verify/[token].js
+++ frontend/pages/api/verify/[token].js
@@ -1,80 +1,144 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from '@/lib/prisma';
+import { redirect } from 'next/navigation';
+import { NextRequest } from 'next/server';
 
-const prisma = new PrismaClient();
+export const GET = async (_request, { params }) => {
+    const { token } = params;
 
-export default async function handler(req, res) {
-    if (req.method === "GET") {
-        const { token } = req.query;
-
-        if (!token) {
-            return res.status(400).json({ message: "Token is missing." });
-        }
-
-        try {
-            const user = await prisma.user.findFirst({
-                where: {
-                    VerificationTokens: {
-                        some: {
-                            AND: [
-                                {
-                                    activatedAt: null,
-                                },
-                                {
-                                    createdAt: {
-                                        gt: new Date(Date.now() - 24 * 60 * 60 * 1000), // 24 hours ago
-                                    },
-                                },
-                                {
-                                    token,
-                                },
-                            ],
+    const user = await prisma.user.findFirst({
+        where: {
+            VerificationTokens: {
+                some: {
+                    AND: [
+                        {
+                            activatedAt: null,
                         },
-                    },
+                        {
+                            createdAt: {
+                                gt: new Date(Date.now() - 24 * 60 * 60 * 1000), // 24 hours ago
+                            },
+                        },
+                        {
+                            token,
+                        },
+                    ],
                 },
-            });
+            },
+        },
+    });
 
-            if (!user) {
-                console.error("There has been an error: Token is invalid or expired");
-                throw new Error('Token is invalid or expired');
-            }
-
-            // Update user status to verified
-            await prisma.user.update({
-                where: {
-                    id: user.id,
-                },
-                data: {
-                    isVerified: true,
-                },
-            });
-
-            await prisma.verificationToken.update({
-                where: {
-                    token,
-                },
-                data: {
-                    activatedAt: new Date(),
-                },
-            });
-
-            // Disconnect Prisma client before sending the response
-            await prisma.$disconnect();
-
-            // Redirect immediately
-            res.writeHead(302, { Location: 'https://forgedmart.com/api/auth/signin' });
-            res.end();
-        } catch (error) {
-            console.error(error);
-            if (error.message === 'Token is invalid or expired') {
-                return res.status(400).json({ error: 'Token is invalid or expired' });
-            } else {
-                return res.status(500).json({ error: `An error occurred during verification: ${error.message}` });
-            }
-        }
+    if (!user) {
+        throw new Error('Token is invalid or expired');
     }
 
-    return res.status(404).json({ message: "Not Found" });
-}
+    await prisma.user.update({
+        where: {
+            id: user.id,
+        },
+        data: {
+            isVerified: true,
+        },
+    });
+
+    await prisma.verificationToken.update({
+        where: {
+            token,
+        },
+        data: {
+            activatedAt: new Date(),
+        },
+    });
+
+    redirect('/api/auth/signin');
+};
+
+
+
+
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default async function handler(req, res) {
+//     if (req.method === "GET") {
+//         const { token } = req.query;
+
+//         if (!token) {
+//             return res.status(400).json({ message: "Token is missing." });
+//         }
+
+//         try {
+//             if (token) {
+//                 await SendNewEmail({ firstName, email, token: token.token, userId: token.userId });
+//             }
+
+
+
+
+//             const user = await prisma.user.findFirst({
+//                 where: {
+//                     VerificationTokens: {
+//                         some: {
+//                             AND: [
+//                                 {
+//                                     activatedAt: null,
+//                                 },
+//                                 {
+//                                     createdAt: {
+//                                         gt: new Date(Date.now() - 24 * 60 * 60 * 1000), // 24 hours ago
+//                                     },
+//                                 },
+//                                 {
+//                                     token,
+//                                 },
+//                             ],
+//                         },
+//                     },
+//                 },
+//             });
+
+//             if (!user) {
+//                 console.error("There has been an error: Token is invalid or expired");
+//                 throw new Error('Token is invalid or expired');
+//             }
+
+//             // Update user status to verified
+//             await prisma.user.update({
+//                 where: {
+//                     id: user.id,
+//                 },
+//                 data: {
+//                     isVerified: true,
+//                 },
+//             });
+
+//             await prisma.verificationToken.update({
+//                 where: {
+//                     token,
+//                 },
+//                 data: {
+//                     activatedAt: new Date(),
+//                 },
+//             });
+
+//             // Disconnect Prisma client before sending the response
+//             await prisma.$disconnect();
+
+//             // Redirect immediately
+//             res.writeHead(302, { Location: 'https://forgedmart.com/api/auth/signin' });
+//             res.end();
+//         } catch (error) {
+//             console.error(error);
+//             if (error.message === 'Token is invalid or expired') {
+//                 return res.status(400).json({ error: 'Token is invalid or expired' });
+//             } else {
+//                 return res.status(500).json({ error: `An error occurred during verification: ${error.message}` });
+//             }
+//         }
+//     }
+
+//     return res.status(404).json({ message: "Not Found" });
+// }
 
 
 // // Example verification API route
