53f1ea743625fda3f215f9a7729c6bf3689ed58f
PublicTrades <info@publictrades.com>
2023-11-29 15:29:45 -0500

    resolving resolve

--- frontend/pages/api/verify/[token].js
+++ frontend/pages/api/verify/[token].js
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { resolve } from 'url';
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 
