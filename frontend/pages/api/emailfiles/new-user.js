b61b3039d7f39314df4b8f0d0e7ce6cbfc9f540f
PublicTrades <info@publictrades.com>
2023-11-29 14:17:13 -0500

    edit to email

--- frontend/pages/api/emailfiles/new-user.js
+++ frontend/pages/api/emailfiles/new-user.js
@@ -8,8 +8,8 @@ export default function Email({ firstName, token }) {
         <p>Hello {firstName},</p>
         <p>Hope this email finds you well.</p>
         <p>Thanks for signing up. Please verify your account by clicking this link:</p>
-        <p><a href={`http://localhost:3000/api/verify/${token}`}>Please verify your account</a></p>
-        {/* <p><a href={`https://forgedmart.com/api/verify/${token}`}>Please verify your account</a></p> */}
+        <p><a href={`http://localhost:3000/api/verify/${token}`}>Please verify your account(Local)</a></p>
+        <p><a href={`https://forgedmart.com/api/verify/${token}`}>Please verify your account</a></p>
 
         <p>Sincerely,</p>
         <p>ForgedMart Team</p>
