35c71774c01cfda87bd91f693f1679b71de7137b
PublicTrades <info@publictrades.com>
2023-12-06 00:19:03 -0500

    modified fetchTeam PRODUCTION

--- frontend/pages/api/fetch-allTeam.js
+++ frontend/pages/api/fetch-allTeam.js
@@ -5,16 +5,16 @@ import { authOptions } from "./auth/[...nextauth]";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-    const session = await getServerSession(req, res, authOptions);
-    const managerEmail = session.user.email;
+    try {
+        const session = await getServerSession(req, res, authOptions);
+        const managerEmail = session?.user?.email;
 
-    if (!managerEmail || !session) {
-        throw new Error('There is no session or email');
-    }
+        if (!managerEmail || !session) {
+            throw new Error('There is no session or email');
+        }
 
-    if (req.method === 'GET') {
-        try {
-            const user = await prisma.user.findUnique({
+        if (req.method === 'GET') {
+            const userPromise = prisma.user.findUnique({
                 where: {
                     email: managerEmail,
                 },
@@ -23,18 +23,30 @@ export default async function handler(req, res) {
                 },
             });
 
-            console.log(prisma.$queryRaw`SELECT * FROM "User" WHERE ...`);
+            const [user, team] = await Promise.all([userPromise, userPromise.Team]);
 
-            if (user && user.Team) {
-                res.status(200).json({ message: 'Successful', user, team: user.Team });
+            if (user && team) {
+                res.status(200).json({ message: 'Successful', user, team });
             } else {
                 res.status(404).json({ message: 'Team not found for the manager' });
             }
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: `There is an error: ${error.message || error}` });
+        } else {
+            console.log("Request not valid");
+            res.status(400).json({ message: 'Invalid request method' });
         }
-    } else {
-        console.log("Request not valid");
+    } catch (error) {
+        console.error('Error in API route:', error);
+
+        // Additional logging for Prisma promises
+        if (error instanceof PrismaClient.Prisma.PrismaClientUnknownRequestError) {
+            console.error('Prisma error details:', {
+                error: error,
+                clientVersion: prisma.$clientVersion,
+            });
+        }
+
+        res.status(500).json({ message: `There is an error: ${error.message || error}` });
+    } finally {
+        await prisma.$disconnect(); // Disconnect Prisma Client after the query
     }
 }
