cb76f5a7e4a19cb37eecefca67d7cd7db4b250b8
PublicTrades <info@publictrades.com>
2023-12-06 12:15:08 -0500

    css debugging mt

--- frontend/components/Creators.js
+++ frontend/components/Creators.js
@@ -12,7 +12,7 @@ export default function Creators() {
     return (
         <div className="bg-white">
             <div className="apply-margin mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-                <div>
+                <div id="maap">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Why Messaging as a Platform</h2>
                     <h3 className="mt-4 text-gray-500">
                         Messaging has evolved far beyond simple text conversations; it has become a powerful tool for content creators to connect with their followers in a more personalized and interactive way.
--- frontend/components/Features.js
+++ frontend/components/Features.js
@@ -14,7 +14,7 @@ export default function Features() {
     return (
         <div className="bg-white">
             <div className="apply-margin mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
-                <div>
+                <div id="prod-feat">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Product and Features</h2>
                     <h3 className="mt-4 text-gray-500">
                         Email Conversational Tool is an AI-powered marketing tool that helps businesses improve their email
