98dd09a9d04b5522d41b7e6d57fad44f8fa7b181
PublicTrades <info@publictrades.com>
2023-12-15 03:33:16 -0500

    updated template once again

--- frontend/pages/api/emailfiles/team-members-invite.js
+++ frontend/pages/api/emailfiles/team-members-invite.js
@@ -2,17 +2,18 @@ import { Html } from "@react-email/html";
 import { Body } from "@react-email/body";
 
 export default function TeamEmail({ managerName, email, token, teamManager, createdUsers, brandName }) {
-    console.log('members invitejs Created Users: ', createdUsers);
+    console.log('members invitejs Created Users: ', createdUsers, teamManager, managerName, brandName);
     return (
         <Html>
             <Body>
                 <p>Hello,</p>
                 <p>
-                    Hope this email finds you well. {teamManager != null ? teamManager : managerName} is sending you an invite to join{' '}
-                    {brandName ? `${brandName}'s team` : "their brand's team"} on ForgedMart.
+                    Hope this email finds you well. {brandName ? `${brandName}'s team` : `${teamManager != null ? teamManager : managerName}'s team is`}{' '}
+                    on ForgedMart. {teamManager != null ? teamManager : managerName} is sending you an invite to join.
                 </p>
                 {/* <p><a href={`http://localhost:3000/api/verify/${token}`}>Please verify your account(Local)</a></p> */}
                 <p><a href={`https://forgedmart.com/api/verify/${token}`}>Please join us to accept the invite.</a> 🎉 Welcome to ForgedMart✨</p>
+
 
                 <p>Sincerely,</p>
                 <p>ForgedMart Team</p>
