12e9a97e2a449a94b8142d8dfd705ec8751b98f2
PublicTrades <info@publictrades.com>
2023-12-22 04:45:22 -0500

    fix for react native

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,7 +12,6 @@ const nextConfig = {
 
     return config;
   },
-
   images: {
     domains: ["images.unsplash.com", "lh3.googleusercontent.com", "res.cloudinary.com"],
     remotePatterns: [
@@ -26,6 +25,10 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    // @see https://github.com/facebookexperimental/Recoil/issues/2135#issuecomment-1362197710
+    RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED: "false",
+  }
 };
 
 module.exports = nextConfig;
