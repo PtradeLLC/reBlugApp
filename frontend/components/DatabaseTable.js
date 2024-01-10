1c5045b6e39afd798c2c6a420fc7a59e4d3e0c07
PubTrades <chrisb@publictrades.com>
2024-01-10 16:35:12 -0500

    updated database page

--- frontend/components/DatabaseTable.js
+++ frontend/components/DatabaseTable.js
@@ -55,17 +55,17 @@ export default function Example() {
 
     return (
         <>
-            <main className="p-4 md:ml-20 h-auto pt-10">
+            <main className=" md:ml-10 h-auto pt-10 max-w-5xl">
                 <div>
-                    <h1 className='text-4xl text-gray-700'>Database</h1>
+                    <h1 className='text-3xl text-gray-700'>Database</h1>
                 </div>
                 <div className='m-10'>
                     <figure className="max-w-screen-md mx-auto text-center">
-                        <svg className="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+                        <svg className="w-7 h-7 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
                             <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z" />
                         </svg>
                         <blockquote>
-                            <p className="text-xl italic font-light text-gray-700 dark:text-white"> ForgedMart prioritizes the security and privacy of your sensitive information. From your campaign emails to supporting documents. Rather than saving data in our database, you can seamlessly connect your data through trusted sources like Google Sheets. Your data flows through our app to provide you with a secure and efficient way to enhance your user experience while maintaining the utmost confidentiality of your valuable data.</p>
+                            <p className="text-l italic font-light text-gray-700 dark:text-white"> ForgedMart prioritizes the security and privacy of your sensitive information. From your campaign emails to supporting documents. Rather than saving data in our database, you can seamlessly connect your data through trusted sources like Google Sheets. Your data flows through our app to provide you with a secure and efficient way to enhance your user experience while maintaining the utmost confidentiality of your valuable data.</p>
                         </blockquote>
                         <figcaption className="flex items-center justify-center mt-6 space-x-3 rtl:space-x-reverse">
                             <img className="w-6 h-6 rounded-full" src="/images/pete.jpg" alt="profile picture" />
