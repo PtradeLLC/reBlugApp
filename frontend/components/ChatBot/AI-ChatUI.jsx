a04a85abcde4e7e09125274831b9605b29a9d948
PubTrades <chrisb@publictrades.com>
2024-02-09 10:59:58 -0500

    updated chat

--- frontend/components/ChatBot/AI-ChatUI.jsx
+++ frontend/components/ChatBot/AI-ChatUI.jsx
@@ -172,11 +172,29 @@ const ChatUI = ({ isOpen, setIsOpen, postContent }) => {
                       </div>
 
                       <div className="mt-4 flex w-full gap-x-2 overflow-x-auto whitespace-nowrap text-xs text-slate-600 dark:text-slate-300 sm:text-sm">
-                        <button className="rounded-lg bg-slate-200 p-2 hover:bg-slate-600 hover:text-slate-200 dark:bg-slate-800 dark:hover:bg-slate-600 dark:hover:text-slate-50">
+                        <button className="flex item-center rounded-lg bg-slate-200 p-2 hover:bg-slate-600 hover:text-slate-200 dark:bg-slate-800 dark:hover:bg-slate-600 dark:hover:text-slate-50">
+                          <svg
+                            class="w-6 h-6 text-gray-800 dark:text-white"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-width="2"
+                              d="M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3a2.5 2.5 0 1 1 2-4.5M19.5 17h.5c.6 0 1-.4 1-1a3 3 0 0 0-3-3h-1m0-3a2.5 2.5 0 1 0-2-4.5m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3c0 .6-.4 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+                            />
+                          </svg>
                           Join Author's group
                         </button>
-                        <button className="rounded-lg bg-slate-200 p-2 hover:bg-slate-600 hover:text-slate-200 dark:bg-slate-800 dark:hover:bg-slate-600 dark:hover:text-slate-50">
-                          Sponsor Article
+                        <button className=" flex items-center rounded-lg bg-slate-200 p-2 hover:bg-slate-600 hover:text-slate-200 dark:bg-slate-800 dark:hover:bg-slate-600 dark:hover:text-slate-50">
+                          <img
+                            className="w-5 h-5 mr-1"
+                            src="/images/productreview.png"
+                          />
+                          Review my product
                         </button>
                       </div>
                       <form onSubmit={handleSubmit} className="mt-2">
