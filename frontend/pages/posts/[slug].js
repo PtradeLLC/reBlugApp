5b241869f5b0fc2c1228e82b476a5e8b1ca9f594
PubTrades <chrisb@publictrades.com>
2024-02-09 16:10:41 -0500

    updated div

--- frontend/pages/posts/[slug].js
+++ frontend/pages/posts/[slug].js
@@ -78,7 +78,7 @@ const PostPage = ({ post }) => {
                     </div>
                 </div>
             </div>
-            <div className='flex pr-6 mt-14 text-gray-900 bg-white border w-32 border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>
+            <div className='flex pr-6 mt-14 text-gray-900 bg-white border w-36 border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>
                 <Link className='flex items-center' href="/blogSignUp">
                     <img className='w-7 h-7 mr-1' src='/images/blogpost.png' /> All Posts
                 </Link>
