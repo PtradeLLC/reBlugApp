607d27a32610c855cbf31206948816450cf53fcd
PubTrades <chrisb@publictrades.com>
2024-04-09 14:34:14 -0400

    undo blogChattwo

--- frontend/utils/tabpage.js
+++ frontend/utils/tabpage.js
@@ -1,109 +1,37 @@
-import React from "react";
-import { Accordion, AccordionItem, Avatar } from "@nextui-org/react";
 
-export default function App() {
-    const defaultContent =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+const emailTab = `
+Features:
+1. Co-promotion: Opt in to partner with other brands and influencers in designated categories to reach a wider audience through chatbot recommendations.
+2. Survey: Ditch conventional surveys forms and create AI-powered surveys through conversation. AI-powered surveys are more engaging and efficient, and they can help you collect richer data. 
+3. Chatbot: Answer questions, provide information, customer support using AI powered conversational chatbot in emails / newsletters.
+4. eCommerce: Have chatbot double up as your sales agent by helping prospective customers find the products or services they're looking for, and guide them through the purchase process with this tool.
+5. Reporting: Collect rich insight on your campaign, what is working, and what's not, and how you can improve to reach your goals quicker through the outcomes tab.
+6. SMS: If the conversation needs to be transferred to a human representative, the chatbot can connect the user to them via SMS messages.
+`;
 
-    return (
-        <Accordion selectionMode="multiple">
-            <AccordionItem
-                key="1"
-                aria-label="Chung Miller"
-                startContent={
-                    <Avatar
-                        isBordered
-                        color="primary"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-                    />
-                }
-                subtitle="4 unread messages"
-                title="Chung Miller"
-            >
-                {defaultContent}
-            </AccordionItem>
-            <AccordionItem
-                key="2"
-                aria-label="Janelle Lenard"
-                startContent={
-                    <Avatar
-                        isBordered
-                        color="success"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                    />
-                }
-                subtitle="3 incompleted steps"
-                title="Janelle Lenard"
-            >
-                {defaultContent}
-            </AccordionItem>
-            <AccordionItem
-                key="3"
-                aria-label="Zoey Lang"
-                startContent={
-                    <Avatar
-                        isBordered
-                        color="warning"
-                        radius="lg"
-                        src="https://i.pravatar.cc/150?u=a04258114e29026702d"
-                    />
-                }
-                subtitle={
-                    <p className="flex">
-                        2 issues to<span className="text-primary ml-1">fix now</span>
-                    </p>
-                }
-                title="Zoey Lang"
-            >
-                {defaultContent}
-            </AccordionItem>
-        </Accordion>
-    );
-}
+const blogTab = `
+Features:
+1. Article Assistant: This AI-powered tool enables you to include conversational chatbot seamlessly on your blog page.
+2. Directory Listing: Directory is a social directory designed to connect you with other like-minded bloggers as well as you with brands. By harnessing this service, you can build Strategic Alliances, and benefit from authentic reviews/recommendations from trusted voices in your industry. 
+3. Auto Pilot: Answer questions, provide information, customer support using AI powered conversational chatbot in emails / newsletters.
+4. eCommerce: Connect and accept sponsorships directly from brands looking to form strategic alliances.
+5. Reporting: Collect rich insight on your blog articles, how readers interact with your contents, and consume your stories.
+6. Research Tool: Enable tool for readers to interact with AI and conduct further research on your articles in real time while they are on the page.
+`;
 
+const toolsArray = [
+    {
+        name: "Start Blogging ",
+        category: "Tool",
+        isActive: false,
+        content: "Article Assistant is not enable yet.",
+    },
+];
 
+const marketing_tab = `There are no campaigns to track. Please start a campaign.`;
+const tools_tab = toolsArray;
+const outcomes_tab = `There are no campaigns to track. Please start a campaign.`;
+const articles_tab = `You have not written an article yet.`;
+const sposorship_tab = `You currently have no sponsorship inquiry.`;
 
-
-
-
-// import { Grid, Flex, Text } from '@radix-ui/themes'
-
-
-// const emailTab = `
-// Features:
-// 1. Co-promotion: Opt in to partner with other brands and influencers in designated categories to reach a wider audience through chatbot recommendations.
-// 2. Survey: Ditch conventional surveys forms and create AI-powered surveys through conversation. AI-powered surveys are more engaging and efficient, and they can help you collect richer data. 
-// 3. Chatbot: Answer questions, provide information, customer support using AI powered conversational chatbot in emails / newsletters.
-// 4. eCommerce: Have chatbot double up as your sales agent by helping prospective customers find the products or services they're looking for, and guide them through the purchase process with this tool.
-// 5. Reporting: Collect rich insight on your campaign, what is working, and what's not, and how you can improve to reach your goals quicker through the outcomes tab.
-// 6. SMS: If the conversation needs to be transferred to a human representative, the chatbot can connect the user to them via SMS messages.
-// `;
-
-// const blogTab = `
-// Features:
-// 1. Article Assistant: This AI-powered tool enables you to include conversational chatbot seamlessly on your blog page.
-// 2. Directory Listing: Directory is a social directory designed to connect you with other like-minded bloggers as well as you with brands. By harnessing this service, you can build Strategic Alliances, and benefit from authentic reviews/recommendations from trusted voices in your industry. 
-// 3. Auto Pilot: Answer questions, provide information, customer support using AI powered conversational chatbot in emails / newsletters.
-// 4. eCommerce: Connect and accept sponsorships directly from brands looking to form strategic alliances.
-// 5. Reporting: Collect rich insight on your blog articles, how readers interact with your contents, and consume your stories.
-// 6. Research Tool: Enable tool for readers to interact with AI and conduct further research on your articles in real time while they are on the page.
-// `;
-
-// const toolsArray = [
-//     {
-//         name: "Start Blogging ",
-//         category: "Tool",
-//         isActive: false,
-//         content: "Article Assistant is not enable yet.",
-//     },
-// ];
-
-// const marketing_tab = `There are no campaigns to track. Please start a campaign.`;
-// const tools_tab = toolsArray;
-// const outcomes_tab = `There are no campaigns to track. Please start a campaign.`;
-// const articles_tab = `You have not written an article yet.`;
-// const sposorship_tab = `You currently have no sponsorship inquiry.`;
-
-// export { emailTab, marketing_tab, outcomes_tab, blogTab, articles_tab, sposorship_tab, tools_tab, toolsArray }
+export { emailTab, marketing_tab, outcomes_tab, blogTab, articles_tab, sposorship_tab, tools_tab, toolsArray }
