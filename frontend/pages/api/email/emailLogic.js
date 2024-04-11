716aed6ecb5b55b4ccb870849b8057c0adce92f0
PubTrades <chrisb@publictrades.com>
2024-04-11 13:13:59 -0400

    updated env

--- frontend/pages/api/email/emailLogic.js
+++ frontend/pages/api/email/emailLogic.js
@@ -112,9 +112,7 @@ export default async function handler(req, res) {
 
     } catch (error) {
       console.error(error);
-      return res
-        .status(500)
-        .json({ error: `An error occurred during registration: ${error.message || error}` });
+      return res.status(500).json({ error: `An error occurred during registration: ${error.message || error}` });
     } finally {
       await prisma.$disconnect();
     }
