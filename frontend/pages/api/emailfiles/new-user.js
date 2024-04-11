53f5fae83ceb2d7ec67df50322fe37d2d11c22e8
PubTrades <chrisb@publictrades.com>
2024-04-11 12:43:45 -0400

    updated resend

--- frontend/pages/api/emailfiles/new-user.js
+++ frontend/pages/api/emailfiles/new-user.js
@@ -12,7 +12,7 @@ export default function Email({ firstName, token }) {
         <p>Hope this email finds you well.</p>
         <p>Thanks for signing up. Please verify your account by clicking this link:</p>
         <p><a href={verificationLink}>Please verify your account</a></p>
-        {/* <p><a href={`http://localhost:3000/api/verify/${token}`}>Please verify your account</a></p> */}
+        {/* <p><a href={`https://www.reblug.com/api/verify/${token}`}>Please verify your account</a></p> */}
 
         <p>Sincerely,</p>
         <p>reBlug Team</p>
