f88b7fef726ed47245e27c32415ac01bf3299b3b
PubTrades <chrisb@publictrades.com>
2024-04-12 10:03:52 -0400

    added video

--- frontend/components/Blogs/Write.js
+++ frontend/components/Blogs/Write.js
@@ -25,7 +25,7 @@ const Compose = ({ showModal, setShowModal }) => {
             [{ 'header': [1, 2, false] }],
             ['bold', 'italic', 'underline', 'strike', 'blockquote'],
             [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-            ['link', 'image'], { size: ['huge'] }
+            ['link', 'image', 'video'], { size: ['huge'] }
             ['clean']
         ],
     },
