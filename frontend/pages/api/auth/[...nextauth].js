3d37bb6fd4895e8fd3ceec6b00da88c2efef5c06
PubTrades <chrisb@publictrades.com>
2024-04-17 02:44:18 -0400

    fix for linkedIn

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -27,7 +27,22 @@ export const authOptions = {
         }),
         LinkedInProvider({
             clientId: process.env.LINKEDIN_CLIENT_ID,
-            clientSecret: process.env.LINKEDIN_CLIENT_SECRET
+            clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+            authorization: {
+                params: { scope: 'openid profile email' },
+            },
+            issuer: 'https://www.linkedin.com',
+            jwks_endpoint: 'https://www.linkedin.com/oauth/openid/jwks',
+            profile(profile, tokens) {
+                const defaultImage =
+                    'https://cdn-icons-png.flaticon.com/512/174/174857.png';
+                return {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture ?? defaultImage,
+                };
+            },
         }),
         FacebookProvider({
             clientId: process.env.FACEBOOK_CLIENT_ID,
