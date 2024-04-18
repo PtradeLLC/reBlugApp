c1076b377c4a3c2e183d50de961137414f78056c
PubTrades <chrisb@publictrades.com>
2024-04-18 16:56:37 -0400

    finally fixed

--- frontend/components/ActivityCover.js
+++ frontend/components/ActivityCover.js
@@ -5,24 +5,12 @@ import Calendar from "./Calendar";
 export default function AllActiveComponent() {
     return (
         <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-            <Card className="col-span-12 sm:col-span-4 h-[300px]">
+            <Card className="col-span-12 sm:col-span-4 md:col-span-6 h-[300px]">
                 <CardHeader className="absolute z-10 top-1 flex-col !items-start">
                     <p className="text-tiny text-white/60 uppercase font-bold">You Calendar</p>
                     <h4 className="text-white font-medium text-large">Stream the Acme event</h4>
                 </CardHeader>
                 <Calendar />
-            </Card>
-            <Card className="col-span-12 sm:col-span-4 h-[300px]">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                    <p className="text-tiny text-white/60 uppercase font-bold">Plant a tree</p>
-                    <h4 className="text-white font-medium text-large">Contribute to the planet</h4>
-                </CardHeader>
-                <Image
-                    removeWrapper
-                    alt="Card background"
-                    className="z-0 w-full h-full object-cover"
-                    src="/images/7venBlog.jpg"
-                />
             </Card>
             <Card className="col-span-12 sm:col-span-4 h-[300px]">
                 <CardHeader className="absolute z-10 top-1 flex-col !items-start">
