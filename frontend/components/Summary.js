4de642049d21bef80deba32d7fb33f26593036f0
PubTrades <chrisb@publictrades.com>
2024-04-18 10:53:53 -0400

    added tabs

--- frontend/components/Summary.js
+++ frontend/components/Summary.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Square3Stack3DIcon } from '@heroicons/react/20/solid';
+import ContentTabs from "./ContentTabs";
 
 const statuses = { Completed: 'text-green-400 bg-green-400/10', Error: 'text-rose-400 bg-rose-400/10' }
 
@@ -46,83 +47,7 @@ export default function Summary() {
             <span className="shadow-xl border-b border-white/10 bg-slate-200">
                 <h2 className="px-4 text-base font-semibold leading-7 flex text-black sm:px-6 lg:px-8"><Square3Stack3DIcon className="text-base px-1 w-6 h-6 text-neutral-400" />Latest activity</h2>
             </span>
-            <table className="mt-6 w-full whitespace-nowrap text-left">
-                <colgroup>
-                    <col className="w-full sm:w-4/12" />
-                    <col className="lg:w-4/12" />
-                    <col className="lg:w-2/12" />
-                    <col className="lg:w-1/12" />
-                    <col className="lg:w-1/12" />
-                </colgroup>
-                <thead className="border-b border-white/10 text-sm shadow-md leading-6 text-black">
-                    <tr>
-                        <th scope="col" className="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8">
-                            Team
-                        </th>
-                        <th scope="col" className="hidden py-2 pl-0 pr-8 font-semibold sm:table-cell">
-                            Engaged
-                        </th>
-                        <th scope="col" className="py-2 pl-0 pr-4 text-right font-semibold sm:pr-8 sm:text-left lg:pr-20">
-                            Status
-                        </th>
-                        <th scope="col" className="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20">
-                            Duration
-                        </th>
-                        <th scope="col" className="hidden py-2 pl-0 pr-4 text-right font-semibold sm:table-cell sm:pr-6 lg:pr-8">
-                            Deployed at
-                        </th>
-                    </tr>
-                </thead>
-                <tbody className="divide-y divide-white/5">
-                    {activityItems.length > 0 ?
-                        activityItems.map((item) => (
-                            <tr key={item['engaged']}>
-                                <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
-                                    <div className="flex items-center gap-x-4">
-                                        <img src={item.user.imageUrl} alt="" className="h-8 w-8 rounded-full bg-gray-800" />
-                                        <div className="truncate text-sm font-medium leading-6 text-black">{item.user.name}</div>
-                                    </div>
-                                </td>
-                                <td className="hidden py-4 pl-0 pr-4 sm:table-cell sm:pr-8">
-                                    <div className="flex gap-x-3">
-                                        <div className="font-mono text-sm leading-6 text-gray-400">{item['engaged']}</div>
-                                        <div className="rounded-md bg-white border px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-white/10">
-                                            {item.branch}
-                                        </div>
-                                    </div>
-                                </td>
-                                <td className="py-4 pl-0 pr-4 text-sm leading-6 sm:pr-8 lg:pr-20">
-                                    <div className="flex items-center justify-end gap-x-2 sm:justify-start">
-                                        <time className="text-gray-400 sm:hidden" dateTime={item.dateTime}>
-                                            {item.date}
-                                        </time>
-                                        <div className={classNames(statuses[item.status], 'flex-none rounded-full p-1')}>
-                                            <div className="h-1.5 w-1.5 rounded-full bg-current" />
-                                        </div>
-                                        <div className="hidden text-black sm:block">{item.status}</div>
-                                    </div>
-                                </td>
-                                <td className="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">
-                                    {item.duration}
-                                </td>
-                                <td className="hidden py-4 pl-0 pr-4 text-right text-sm leading-6 text-gray-400 sm:table-cell sm:pr-6 lg:pr-8">
-                                    <time dateTime={item.dateTime}>{item.date}</time>
-                                </td>
-                            </tr>
-                        ))
-                        :
-                        <>
-                            <tr>
-                                <td className="flex mt-2 justify-center items-center">
-
-                                </td>
-
-                            </tr>
-
-                        </>
-                    }
-                </tbody>
-            </table>
+            <ContentTabs />
             <div className="bg-slate-50 rounded-md my-8 p-3 mx-2">
                 {summary}
             </div>
