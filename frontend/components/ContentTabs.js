4c62f2acfb48c00e783173606b76bb3a569ba708
PubTrades <chrisb@publictrades.com>
2024-04-18 11:30:12 -0400

    set Activities

--- frontend/components/ContentTabs.js
+++ frontend/components/ContentTabs.js
@@ -3,7 +3,9 @@ import { Tabs, Tab, Card, CardBody, Switch } from "@nextui-org/react";
 
 export default function ContentTabs() {
     const [isVertical, setIsVertical] = useState(true);
-    const [activeKey, setActiveKey] = useState("all");
+    const [allActivities, setAllActivities] = useState(false);
+    const [allEmail, setAllEmail] = useState(false);
+    const [allBlogs, setAllBlogs] = useState(false);
     const [noActivity, setNoActivity] = useState(null);
 
 
@@ -16,23 +18,39 @@ export default function ContentTabs() {
                 <Tabs aria-label="Options" isVertical={isVertical}>
                     <Tab key="all" title="All Activities">
                         <Card>
-                            <CardBody>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                            </CardBody>
+                            {allActivities ? (
+                                <CardBody>
+                                    "All"
+                                </CardBody>
+                            ) : (
+                                <CardBody>
+                                    You have no activity
+                                </CardBody>
+                            )}
                         </Card>
                     </Tab>
                     <Tab key="email" title="Email Activities">
-                        <Card>
+                        {allEmail ? (
                             <CardBody>
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                "All"
                             </CardBody>
-                        </Card>
+                        ) : (
+                            <CardBody>
+                                You have no activity
+                            </CardBody>
+                        )}
                     </Tab>
                     <Tab key="blogs" title="Blog Activities">
                         <Card>
-                            <CardBody>
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </CardBody>
+                            {allBlogs ? (
+                                <CardBody>
+                                    "All"
+                                </CardBody>
+                            ) : (
+                                <CardBody>
+                                    You have no activity
+                                </CardBody>
+                            )}
                         </Card>
                     </Tab>
                 </Tabs>
