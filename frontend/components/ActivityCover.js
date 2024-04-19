e64c794dfb4382826db5885822e82e23ead02a3f
PubTrades <chrisb@publictrades.com>
2024-04-19 02:48:23 -0400

    idea

--- frontend/components/ActivityCover.js
+++ frontend/components/ActivityCover.js
@@ -74,14 +74,14 @@ export default function AllActiveComponent() {
             </Card>
             <Card isFooterBlurred className="w-full col-span-12 sm:col-span-7">
                 <CardHeader className="absolute z-10 top-1 flex-col items-start">
-                    <p className="text-tiny text-white/60 uppercase font-bold">Your day your way</p>
+                    <p className="text-tiny text-white/60 uppercase font-bold">reBlug Lab</p>
                     <h4 className="text-white/90 font-medium text-xl">Your checklist for better sleep</h4>
                 </CardHeader>
                 <Image
                     removeWrapper
                     alt="Relaxing app background"
                     className="z-0 w-full h-full object-cover"
-                    src="/images/pete.jpg"
+                    src="/images/ideas.jpg"
                 />
                 <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
                     <div className="flex flex-grow gap-2 items-center">
