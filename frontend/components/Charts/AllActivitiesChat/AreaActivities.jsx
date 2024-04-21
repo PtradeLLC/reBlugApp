32a5066ea72d5dd064e607f0640347c9e2cc0482
PubTrades <chrisb@publictrades.com>
2024-04-21 11:11:02 -0400

    min

--- frontend/components/Charts/AllActivitiesChat/AreaActivities.jsx
+++ frontend/components/Charts/AllActivitiesChat/AreaActivities.jsx
@@ -14,18 +14,18 @@ const AreaActivities = () => {
           <p className="text-base font-normal text-gray-500 dark:text-gray-400">
             Something goes here
           </p>
-        </div>
-        <div className="flex items-center px-2.5 py-0.5 text-base font-semibold text-yellow-500 dark:text-yellow-500 text-center">
-          0%
-          <span
-            className="w-3 h-3 ms-1"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 10 14"
-          >
-            <span>--</span>
-          </span>
+          <div className="flex items-center px-2.5 py-0.5 text-base font-semibold text-yellow-500 dark:text-yellow-500 text-center">
+            0%
+            <span
+              className="w-3 h-3 ms-1"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 14"
+            >
+              <span>--</span>
+            </span>
+          </div>
         </div>
       </div>
       <div id="area-chart"></div>
@@ -80,35 +80,11 @@ const AreaActivities = () => {
                   Something goes here
                 </a>
               </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Something goes here
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Something goes here
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Something goes here
-                </a>
-              </li>
             </ul>
           </div>
           <a
             href="#"
-            className="uppercase text-sm font-semibold inline-flex items-center rounded-lg text-red-600 hover:text-red-700 dark:hover:text-red-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2"
+            className="uppercase text-sm font-thin inline-flex items-center rounded-lg text-red-600 hover:text-red-700 dark:hover:text-red-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2"
           >
             Something goes here
             <svg
