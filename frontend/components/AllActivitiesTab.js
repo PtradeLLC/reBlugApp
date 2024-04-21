efbd05d29e17036e09d2b2067bf6a3ec1f739c63
PubTrades <chrisb@publictrades.com>
2024-04-21 12:13:32 -0400

    I

--- frontend/components/AllActivitiesTab.js
+++ frontend/components/AllActivitiesTab.js
@@ -9,9 +9,11 @@ const AllActiveComponent = () => {
         <>
             <span className="py-2 mt-2 font-semibold px-2 text-md">All Activities</span>
             <div class="grid grid-cols-2 gap-4 w-full">
-                <div class="col-span-2..."><AreaActivities /></div>
-                <div class="col-span-2..."><Earnings /></div>
-                <div class="col-span-2 ..."><BlogActivities /></div>
+                <div class="grid grid-cols-2 gap-4 w-full">
+                    <div class="col-span-2 md:col-span-1"><AreaActivities /></div>
+                    <div class="col-span-2 md:col-span-1"><Earnings /></div>
+                </div>
+                <div class="col-span-2"><BlogActivities /></div>
             </div>
         </>
     )
