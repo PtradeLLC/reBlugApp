792106ff0c9bbe18e197509349aa1b279b22a985
PubTrades <chrisb@publictrades.com>
2024-04-22 16:13:42 -0400

    ssWesssesxsxs

--- frontend/components/BlogsTab.js
+++ frontend/components/BlogsTab.js
@@ -51,7 +51,7 @@ const BlogsTab = () => {
                 </ul>
                 <div className="p-6 bg-gray-50 text-medium text-gray-500 dark:text-gray-400 dark:bg-gray-800 rounded-lg w-full">
                     <h3 className="text-lg font-bold text-gray-900 dark:text-white mb-2">Blog Comments</h3>
-                    <CommentBox/>
+                    {/* <CommentBox/> */}
                 </div>
             </div>
 
