80306ea2a7a3f0edfdc09efec56ea37488b99f69
PubTrades <chrisb@publictrades.com>
2024-04-22 12:11:25 -0400

    bcdycydw

--- frontend/components/Charts/AllActivitiesChat/AreaActivities.jsx
+++ frontend/components/Charts/AllActivitiesChat/AreaActivities.jsx
@@ -4,19 +4,19 @@ import { options } from "./chartData/areaOptionsData";
 const AreaActivities = () => {
   const [defaultReport, setDefaultReport] = useState(null);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const chartCanvas = document.getElementById("area-chart");
-      if (chartCanvas && typeof ApexCharts !== "undefined") {
-        const chart = new ApexCharts(chartCanvas, options);
-        chart.render();
-      }
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     const chartCanvas = document.getElementById("area-chart");
+  //     if (chartCanvas && typeof ApexCharts !== "undefined") {
+  //       const chart = new ApexCharts(chartCanvas, options);
+  //       chart.render();
+  //     }
+  //   }
+  // }, []);
 
-  const handleClick = () => {
-    setDefaultReport("Nothing to report yet");
-  };
+  // const handleClick = () => {
+  //   setDefaultReport("Nothing to report yet");
+  // };
 
   return (
     <div className="w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
@@ -36,7 +36,7 @@ const AreaActivities = () => {
           </p>
         </div>
       </div>
-      <div id="area-chart"></div>
+      {/* <div id="area-chart"></div> */}
       <div className="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
         <div className="flex justify-between items-center pt-5">
           <div
@@ -60,7 +60,7 @@ const AreaActivities = () => {
           <button
             onClick={handleClick}
             type="button"
-            value={defaultReport}
+            // value={defaultReport}
             className="text-sm font-thin inline-flex items-center rounded-lg text-red-600 hover:text-red-700 dark:hover:text-red-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2"
           >
             Report
