2ce1439c6e2cb47e64ed80b9c2c297ffba295a33
PubTrades <chrisb@publictrades.com>
2024-06-22 01:46:19 -0400

    domain config

--- eatnow/app/appwrite.js
+++ eatnow/app/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Account, ID } from 'appwrite';
+import { Client, Account, ID, Databases, Storage, Avatars } from 'appwrite';
 
 const client = new Client();
 
@@ -7,4 +7,9 @@ client
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+const avatars = new Avatars(client);
+
+
 export { client, account, ID };
